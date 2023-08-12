b001167b79ee7be75c7d0ec6d7ca2e0333c14446
Robin1-afk <robincastellanosperez@gmail.com>
2023-08-12 10:23:29 -0500

    Agrega proyecto Angular en rama master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Route } from '@angular/router';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { FooterComponent } from './esq/footer/footer.component';
+import { NavbarComponent } from './esq/navbar/navbar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientsComponent,
+    FooterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false, // Cambia a true en production
+    apiUrl: 'https://api.dev.example.com'
+  };
